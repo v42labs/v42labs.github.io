0cb0bb8d0b159cb4df1deb52e41e6183e2dd29a5
Ramesh Nair <ram@hiddentao.com>
2024-05-14 14:11:22 +0100

    77a69bc

---
+++ index.579125c3.js
@@ -0,0 +1,10 @@
+(()=>{
+    window.showBlock = (id)=>{
+        document.getElementById(id).style.opacity = 1;
+    };
+    window.hideBlock = (id)=>{
+        document.getElementById(id).style.opacity = 0;
+    };
+})();
+
+//# sourceMappingURL=index.579125c3.js.map
---
+++ index.e196585e.js
@@ -0,0 +1,2 @@
+window.showBlock=e=>{document.getElementById(e).style.opacity=1},window.hideBlock=e=>{document.getElementById(e).style.opacity=0};
+//# sourceMappingURL=index.e196585e.js.map
